0c80dd13d6cdc228616eb6c4530d2cb6711bbce9
Serezha <sterhov150800@gmail.com>
2020-02-08 15:32:31 +0400

    object

--- src/object/index.js
+++ src/object/index.js
@@ -2,8 +2,8 @@
   Создать и вернуть пустой объект
  */
 function createObject() {
-  let c ={};
-  return c;
+    let c = {};
+    return c;
 }
 
 /**
@@ -20,12 +20,11 @@ function createObject() {
  * @param object
  */
 function getCountOfProp(object) {
-  let a=0;
-  for (let key in object)
-  {
-    a++
-  }
-  return a;
+    let a = 0;
+    for (let key in object) {
+        a++
+    }
+    return a;
 }
 
 /**
@@ -33,8 +32,8 @@ function getCountOfProp(object) {
  * @param obj
  */
 function copyObject(obj) {
-let copy=Object.assign({}, obj);
-return copy;
+    let copy = Object.assign({}, obj);
+    return copy;
 }
 
 /**
@@ -45,9 +44,9 @@ return copy;
  * mergeObject(a, b) = { name: 'Max', age: 18, }
  */
 function mergeObject(objectA, objectB) {
-  let merge = Object.assign ({},objectA, objectB);
+    let merge = Object.assign({}, objectA, objectB);
 
-  return merge;
+    return merge;
 }
 
 /**
@@ -56,7 +55,7 @@ function mergeObject(objectA, objectB) {
  * hasKey(a, 'name') = true
  */
 function hasKey(object, key) {
-return (key in object)
+    return (key in object)
 }
 
 /**
@@ -69,15 +68,89 @@ return (key in object)
  * addToObject(a, key, value) = { name: '1', age: 18 }
  */
 function addToObject(object, key, value) {
-object[key] = value
-return object;
+    object[key] = value
+    return object;
+} **
+Удалить ключ из объекта и вернуть новый
+    *
+    obj1 !== obj2
+let obj1 = { name: 'Max', age: 22 };
+let obj2 = removeKey(obj1, 'name');
+// obj2 ->  { age: 22 }
+/** */
+
+function removeKey(obj, age) {
+    let merge = Object.assign({}, obj);
+    delete merge[age]
+    return merge
 }
 
+/**Создать и вернуть объект, ключи которого есть в обоих объектах
+Придумай название функции
+let obj1 = {
+  firstName: 'Max',
+  lastName: 'Xam',
+  age: 22,
+};
+
+let obj2 = {
+  age: 22,
+  city: 'NY',
+};
+
+let obj3 = функция(obj1, obj2);
+// obj3 -> {  age: 22  }
+/** */
+
+function KeyInObj(obj1, obj2) {
+    let obj3 = {};
+    for (let key in obj1) {
+        if (key in obj2) {
+            obj3[key] = obj1[key]
+        }
+    }
+    return obj3
+}
+
+
+/**Создать и вернуть объект, ключи которого есть только в одном из объектов (разность)
+Придумай название функции
+let obj1 = {
+  firstName: 'Max',
+  lastName: 'Xam',
+  age: 22,
+};
+
+let obj2 = {
+  age: 22,
+  city: 'NY',
+};
+
+let obj3 = функция(obj1, obj2);
+// obj3 -> {  city: 'NY', lastName: 'Xam',  firstName: 'Max',  }
+/** */
+
+function KeyInObjDiff(obj1, obj2) {
+    let obj3 = {}
+    for (let key in obj2) {
+        if (key in obj1) {
+            obj3[key] = obj2[key]
+        }
+    }
+    for (key in obj1) {
+        if (key in obj2) {
+            obj3[key] = obj1[key]
+        }
+    }
+}
+
+
+
 module.exports = {
-  createObject,
-  getCountOfProp,
-  copyObject,
-  mergeObject,
-  hasKey,
-  addToObject,
+    createObject,
+    getCountOfProp,
+    copyObject,
+    mergeObject,
+    hasKey,
+    addToObject,
 };
